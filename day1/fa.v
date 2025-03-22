f750c08df38aedda489335d90e6a437d783969f5
chanra mouli polu <chandramoulipolu09@gmail.com>
2025-03-22 23:14:52 +0530

    Update fa.v

--- day1/fa.v
+++ day1/fa.v
@@ -1,1 +1,14 @@
-
+module full_adder(
+  input a,
+  input b,
+  input cin,
+  output sum,
+  output cout
+);
+  wire w1,w2,w3;
+  assign w1 =a^b;
+  assign sum =w1^cin;
+  assign w2 =a&b;
+  assign w3 =w1&cin;
+  assign c = w3|w2;
+endmodule
