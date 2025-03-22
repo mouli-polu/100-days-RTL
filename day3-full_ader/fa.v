119d5953788cd95bc53a65bc41bd83a791b4c49a
chanra mouli polu <chandramoulipolu09@gmail.com>
2025-03-22 23:56:58 +0530

    fulladder

--- day3-full_ader/fa.v
+++ day3-full_ader/fa.v
@@ -1,1 +1,50 @@
+/*// data flow leve
+module full_adder(
+  input a,
+  input b,
+  input cin,
+  output sum,
+  output cout 
+);
+  wire w1,w2,w3;
+  assign w1 =a^b;
+  assign sum =w1^cin;
+  assign w2 =a&b;
+  assign w3 =w1&cin;
+  assign cout = w3|w2;
+endmodule
+*/
+/*
+//gate level
+module full_adder(
+  input a,
+  input b,
+  input cin,
+  output sum,
+  output cout
+);
+  wire w1,w2,w3;
+  xor(w1,a,b);
+  xor(sum,w1,cin);
+  and(w2,a,b);
+  and (w3,w1,cin);
+  or(cout,w2,w3);
+endmodule
+*/
+/*
+//behaviour level
+module full_adder(
+  input a,
+  input b,
+  input cin,
+  output sum,
+  output cout
+);
 
+  always@(*) begin
+    sum =a^b^cin;
+    cout =(a&b)|(b^a)cin);
+    //{cout,sum}=a+b+cin;
+  end
+endmodule
+*/
